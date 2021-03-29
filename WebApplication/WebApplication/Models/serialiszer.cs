862c9280d1565a921ad7cc081d1830ceb05dec83
Кащеев Илья <tactical949@gmail.com>
2021-03-29 14:29:22 +0600

    установил Newtonsoft и написал класс для десериализации

--- WebApplication/WebApplication/Models/serialiszer.cs
+++ WebApplication/WebApplication/Models/serialiszer.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WebApplication.Models
 {
     public class serialiszer
     {
+        public Parser GetParser() {
+
+            var res = JsonConvert.DeserializeObject<Parser>(File.ReadAllText("data.json"));
+            Console.WriteLine(res);
+            return res;
+        }
     }
 }
+
