51c47307153eae0399b4b79e5dca8535b847692d
Кащеев Илья <tactical949@gmail.com>
2021-03-30 22:23:39 +0600

    заготовка для api/errors и сделал запрос api/errors/count

---
+++ WebApplication/WebApplication/Controllers/errors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    
+    public class errors : ControllerBase
+    {
+        private serialiszer serialiszer = new serialiszer();
+        string error = "";
+        [HttpGet]
+        public string errosInf() {
+            var res = serialiszer.GetParser();
+
+            return error;
+        }
+
+        [HttpGet("count")]
+        public string countInf() {
+            var res = serialiszer.GetParser();
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(res.scan.errorCount);
+            return json;
+        }
+    }
+}
