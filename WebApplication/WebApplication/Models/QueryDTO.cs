f71ab93fd0f489074f9fb429547155bf04c3bed4
Кащеев Илья <tactical949@gmail.com>
2021-03-31 21:01:45 +0600

    модель DTO и запрос api/query/check

---
+++ WebApplication/WebApplication/Models/QueryDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class QueryDTO
+    {
+        public int total { get; set; }
+        public int correct { get; set; }
+        public int errors { get; set; }
+        public List<string> filename { get; set; }
+    }
+}
